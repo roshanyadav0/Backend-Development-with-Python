498df568708dd837d10ccf72aa3fb99815f674bd
Roshan Yadav <118208376+roshanyadav0@users.noreply.github.com>
2026-07-04 07:53:16 +0530

    Delete __init__.py

--- expense_tracker/__init__.py
+++
@@ -1,6 +0,0 @@
-from .models import Expense
-from .tracker import ExpenseTracker
-from .exceptions import ExpenseTrackerError
-
-__all__ = ["Expense", "ExpenseTracker", "ExpenseTrackerError"]
-__version__ = "3.0.0"
